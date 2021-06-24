46146b2ea13861d9302292947e01fc2724480569
Abubakar Diallo <dialloabubakar5861@gmail.com>
2021-06-24 13:14:54 +0100

    feat(frontend): navbar component added

---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='fixed bottom-0 w-full bg-gray-800'>
+      <h1 className='text-white'>Home</h1>
+    </div>
+  )
+}
+
+export default NavBar
