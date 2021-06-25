d7a0332b435bc30eefe49bf21f48fffd59ac2ae3
Abubakar Diallo <dialloabubakar5861@gmail.com>
2021-06-25 05:21:06 +0100

    feat(frontend): icons added as component

---
+++ src/vectors/MoreIcon.js
@@ -0,0 +1,23 @@
+import * as React from 'react'
+
+function MoreIcon (props) {
+  return (
+    <svg
+      width='2em'
+      height='2em'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M2.4 4.8a.8.8 0 100 1.6h19.2a.8.8 0 100-1.6H2.4zM1.6 12a.8.8 0 01.8-.8h19.2a.8.8 0 110 1.6H2.4a.8.8 0 01-.8-.8zm0 6.4a.8.8 0 01.8-.8h19.2a.8.8 0 110 1.6H2.4a.8.8 0 01-.8-.8z'
+        fill='#718596'
+      />
+    </svg>
+  )
+}
+
+export default MoreIcon
